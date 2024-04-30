34770ee83ca62005bd44f4d6b8fbb79821ecc6b8
Anselmo Tadeu Dos Santos Filho <anselmo.filho@nava.com.br>
2024-04-30 07:47:57 -0300

    Início do projeto de criação da tela de login

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className="container">
+        <form>
+            <h1>Acesse o sistema</h1>
+            <input type="email" placeholder="E-mail" />
+        </form>
+    </div>
+  )
+}
+
+export default login
